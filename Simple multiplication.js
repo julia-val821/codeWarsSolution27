84e6d8a25e3142e9ac3f87657776d140c831f394
JuliaVal821 <julia.kolgatina@gmail.com>
2020-09-12 21:31:20 -0400

    Used operator if else

---
+++ Simple multiplication.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+    function simpleMultiplication(number) {
+        if(number % 2) return number * 9;
+        else return number * 8;
+    }
